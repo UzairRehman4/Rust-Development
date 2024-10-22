8b0643d560e9d960805efdbe4f810f1681f46710
Uzair.dev <rehmanuzair43@gmail.com>
2024-10-22 19:49:08 +0500

    loops

--- rust_control_flow/src/main.rs
+++ rust_control_flow/src/main.rs
@@ -1,27 +1,38 @@
-fn main() {
-    // let y = is_even(4);
-    // println!("value of y is {y:?}");
+// fn main() {
+// let y = is_even(4);
+// println!("value of y is {y:?}");
 
-    // if y {
-    //     println!("Fn returned true");
-    // }
-    // let calling = is_even(6);
-    // println!("calling fun value is {calling:?}");
+// if y {
+//     println!("Fn returned true");
+// }
+// let calling = is_even(6);
+// println!("calling fun value is {calling:?}");
 
-    // looping
+// looping
 
-    let mut num = 1;
+// let mut num = 1;
 
-    loop {
-        println!("looping bro {num:?}");
+// loop {
+//     println!("looping bro {num:?}");
 
-        if num == 10 {
-            break;
-        }
+//     if num == 10 {
+//         break;
+//     }
 
-        num = num + num;
-    }
-}
+//     num = num + num;
+// }
+//     let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("The result is {result}");
+// }
 
 // fn is_even(x: i32) -> bool {
 //     if x % 2 == 0 {
@@ -30,3 +41,25 @@ fn main() {
 //         false
 //     }
 // }
+
+fn main() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }   
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
