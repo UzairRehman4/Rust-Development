aaa8eab169f0c8b776457d3fca712dfd72970b89
Uzair.dev <rehmanuzair43@gmail.com>
2024-12-14 01:38:25 +0500

    rust file compresser cli

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let mut s = String::from("Hello");
+    // let mut s = String::from("Hello");
 
-    s.push_str(" Uzair");
+    // s.push_str(" Uzair");
 
-    println!("S = {s}");
+    // println!("S = {s}");
+    let mut x = 10;
+    let y = x;
+
+    x = 20;
+    println!("x = {x}, y = {y}");
 }
